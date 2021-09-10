a469ff9ab23d0a3f777aff5c2b632fa6acbc6b77
Solit0123 <61633743+Solit0123@users.noreply.github.com>
2021-09-09 19:58:35 -0700

    Copy of nextjs1
    dragged and droped all other files to this repo via VS folder

---
+++ blog2/pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+
+    return (
+        <>
+            <Head>
+                <title>First Post Title</title>
+            </Head>
+
+            <h1>First Post</h1>
+        </>
+    )
+};
